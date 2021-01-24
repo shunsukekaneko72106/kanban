9dd6f075892dc3b994f62dde427d8094541da646
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-01-24 15:49:42 +0900

    test
    desk top

--- kanban/views.py
+++ kanban/views.py
@@ -1,12 +1,16 @@
-from django.contrib.auth import login # 追加
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm # 追加
-from django.shortcuts import render, redirect # redirectをインポート
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import DetailView, UpdateView
 
+from .forms import UserForm
 
 
 def index(request):
     return render(request, "kanban/index.html")
+
 
 @login_required
 def home(request):
@@ -22,8 +26,23 @@ def signup(request):
             return redirect("kanban:home")
     else:
         form = UserCreationForm()
-    context = {
-        "form": form
-    }
+        context = {
+            "form": form
+        }
     return render(request, 'kanban/signup.html', context)
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "kanban/users/detail.html"
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = "kanban/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
+
 
