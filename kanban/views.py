78b5841a578265bcaf4acb12ed2c421e9df23486
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-01-30 10:38:09 +0900

    add list function

--- kanban/views.py
+++ kanban/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, resolve_url
-from django.views.generic import DetailView, UpdateView, CreateView
+from django.views.generic import DetailView, UpdateView, CreateView,ListView
 
 from django.urls import reverse_lazy
 from .models import List
@@ -73,5 +73,11 @@ class ListCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+"""リストのクラス"""
+class ListListView(LoginRequiredMixin, ListView):
+    model = List
+    template_name = "kanban/lists/list.html"
 
 
