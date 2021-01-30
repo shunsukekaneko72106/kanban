f158b4906e34a9736582359638a289149f54e025
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2021-01-30 15:10:16 +0900

    all function

--- kanban/views.py
+++ kanban/views.py
@@ -18,15 +18,15 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, resolve_url
+from django.shortcuts import render, redirect, resolve_url, get_object_or_404
 from django.views.generic import DetailView, UpdateView, CreateView, ListView, DeleteView
 
 from django.urls import reverse_lazy
 
-
-from .forms import UserForm, ListForm, CardForm
+from .forms import UserForm, ListForm, CardForm, CardCreateFromHomeForm
 from .mixins import OnlyYouMixin
 from .models import List, Card
+
 
 # render(request, template_name, context)はテンプレートに値（変数）を埋め込んだ結果をHttpResponseに変換する関数
 
@@ -34,10 +34,10 @@ def index(request):
     return render(request, "kanban/index.html")
 
 
-# デコレーターでログインを呼び出し
-@login_required
-def home(request):
-    return render(request, "kanban/home.html")
+
+class HomeView(LoginRequiredMixin, ListView):
+    model = List
+    template_name = "kanban/home.html"
 
 
 def signup(request):
@@ -100,22 +100,25 @@ class ListDetailView(LoginRequiredMixin, DetailView):
     model = List
     template_name = "kanban/lists/detail.html"
 
+
 """リスト編集クラス"""
+
+
 class ListUpdateView(LoginRequiredMixin, UpdateView):
     model = List
     template_name = "kanban/lists/update.html"
     form_class = ListForm
-
-    def get_success_url(self):
-        return resolve_url('kanban:lists_detail', pk=self.kwargs['pk'])
+    success_url = reverse_lazy("kanban:home")
 
 
 """リスト削除クラス"""
+
+
 class ListDeleteView(LoginRequiredMixin, DeleteView):
     model = List
     template_name = "kanban/lists/delete.html"
     form_class = ListForm
-    success_url = reverse_lazy("kanban:lists_list")
+    success_url = reverse_lazy("kanban:home")
 
 
 """カード作成クラス"""
@@ -123,11 +126,12 @@ class CardCreateView(LoginRequiredMixin, CreateView):
     model = Card
     template_name = "kanban/cards/create.html"
     form_class = CardForm
-    success_url = reverse_lazy("kanban:cards_list")
+    success_url = reverse_lazy("kanban:home")
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
 
 
 class CardListView(LoginRequiredMixin, ListView):
@@ -141,17 +145,34 @@ class CardDetailView(LoginRequiredMixin, DetailView):
 
 
 """カード編集クラス"""
+
+
 class CardUpdateView(LoginRequiredMixin, UpdateView):
     model = Card
     template_name = "kanban/cards/update.html"
     form_class = CardForm
+    success_url = reverse_lazy("kanban:home")
 
-    def get_success_url(self):
-        return resolve_url('kanban:cards_detail', pk=self.kwargs['pk'])
 
 """カード削除クラス"""
+
+
 class CardDeleteView(LoginRequiredMixin, DeleteView):
     model = Card
     template_name = "kanban/cards/delete.html"
     form_class = CardForm
-    success_url = reverse_lazy("kanban:cards_list")
+    success_url = reverse_lazy("kanban:home")
+
+
+class CardCreateFromHomeView(LoginRequiredMixin, CreateView):
+    model = Card
+    template_name = "kanban/cards/create.html"
+    form_class = CardCreateFromHomeForm
+    success_url = reverse_lazy("kanban:home")
+
+    def form_valid(self, form):
+        list_pk = self.kwargs['list_pk']
+        list_instance = get_object_or_404(List, pk=list_pk)
+        form.instance.list = list_instance
+        form.instance.user = self.request.user
+        return super().form_valid(form)
